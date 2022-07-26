a4e4a29426addcf512a6b21c99f96a7c4d14b24b
Alesouza7 <alexcostasouza86@yahoo.com>
2022-07-25 23:56:51 -0300

    exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let nomeUsuario = prompt("Digite um nome de usuário: ")
+let comida1 = prompt("Escreva a 1ª comida favorita?")
+let comida2 = prompt("Escreva a 2ª comida favorita?")
+let comida3 = prompt("Escreva a 3ª comida favorita?")
+console.log(`Estas são as comidas favoritas de ${nomeUsuario}
+-${comida1}
+-${comida2}
+-${comida3}`) 
